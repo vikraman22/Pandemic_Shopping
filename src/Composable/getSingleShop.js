dc771c824d5e91833edcb98d6310954df37665ee
Vikram <vikraman2223@gmail.com>
2021-06-21 23:33:45 +0530

    singlestore composable

---
+++ src/Composable/getSingleShop.js
@@ -0,0 +1,35 @@
+import { ref } from 'vue'
+import { projectFirestore } from "../Firebase/config"
+
+const getSingleShop = (id) => {
+
+    const shop = ref(null)
+    const error = ref(null)
+
+    const load = async() => {
+     try
+     {
+         let res = await projectFirestore.collection('store').doc(id).get()
+         
+         if(!res.exists)
+         {
+             throw Error("Store not available")
+         }
+
+         shop.value = {...res.data(), id: res.id}
+
+
+     }
+     catch (err)
+     {
+        error.value = err.message
+     }
+
+
+    }
+
+    return { shop, error, load }
+
+}
+
+export default getSingleShop
